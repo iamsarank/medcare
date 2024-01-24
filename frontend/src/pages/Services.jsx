413eb6e4b957a0a1c49b3a45c0741d006b3ebe20
saravanakumar <iamsarank@gmail.com>
2024-01-24 13:06:18 +0530

    first commit

---
+++ frontend/src/pages/Services.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ServiceList from '../components/Services/ServiceList'
+
+const Services = () => {
+  return (
+    <section>
+          <div className="container">
+            <div className="xl:w-[470px] mx-auto">
+              <h2 className="heading text-center">Our medical services</h2>
+              <p className="text__para text-center">
+                World-class care for everyone. Our health System offers
+                unmatched, expert health care.
+              </p>
+            </div>
+            <ServiceList />
+          </div>
+        </section>
+  )
+}
+
+export default Services
